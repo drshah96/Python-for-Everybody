0e47df8b74b9ee201d10237c362afaf1d94aca03
drshah96 <drshah96@gmail.com>
2020-06-11 18:26:13 -0400

    Using Python to Access Web Data Week 4: Web Surf Program: Parsing Web Pages (with and without SSL Certificates)

--- 3 - Using python to Access Web Data/Python Scripts/WebSurfPrograms.py
+++ 3 - Using python to Access Web Data/Python Scripts/WebSurfPrograms.py
@@ -1,4 +1,4 @@
-### Using URLLIB in Python
+### Retrieving Web Page Using URLLIB in Python
 
 import urllib.request
 
@@ -17,3 +17,35 @@ for line in fhand:
     for word in words:
         counts[word] = counts.get(word, 0) + 1
 print(counts)
+
+### Parsing Web Pages
+import urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter URL: ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+### Parsing Web Pages by ignoring SSL Certificatrd
+import urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL Certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
