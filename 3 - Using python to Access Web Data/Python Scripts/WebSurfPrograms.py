20e4d64a5864b83b835c3df0b3d1d1304b97ecc6
drshah96 <drshah96@gmail.com>
2020-06-11 19:06:18 -0400

    Using Python to Access Web Data Week 4: Web Surf Program: Quiz

--- 3 - Using python to Access Web Data/Python Scripts/WebSurfPrograms.py
+++ 3 - Using python to Access Web Data/Python Scripts/WebSurfPrograms.py
@@ -47,5 +47,31 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
 tags = soup('a')
+print(tags)
 for tag in tags:
     print(tag.get('href', None))
+
+### Quiz
+# 1
+x = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')  # File Handle
+
+# 2
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
+
+import re
+
+tags = '<p>Please click <a href="http://www.dr-chuck.com">here</a></p>'
+y = re.findall('(.+)', tags)
+print(y)
+# http://www.dr-chuck.com
