8d6ab88b595075d11dbcf046838da35826af3a5a
drshah96 <drshah96@gmail.com>
2020-06-12 14:35:35 -0400

    Using Python to Access Web Data Week 6: JSON and REST Architecture: Graded Assignment
    COURSE COMPLETE

--- 3 - Using python to Access Web Data/Python Scripts/JSONandRESTArchitecture.py
+++ 3 - Using python to Access Web Data/Python Scripts/JSONandRESTArchitecture.py
@@ -186,3 +186,89 @@ data = '''
 info = json.loads(data)
 print(len(info))
 print(type(info))
+
+### Graded Assignment 1: Extracting Data from JSON
+import urllib.error
+import json
+
+url_sample = 'http://py4e-data.dr-chuck.net/comments_42.json'
+url = 'http://py4e-data.dr-chuck.net/comments_445372.json'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read().decode()
+print('Retrieved', len(data), 'characters')
+
+try:
+    js = json.loads(data)
+except:
+    js = None
+
+if not js or 'status' not in js or js['status'] != 'OK':
+    print('==== Failure To Retrieve ====')
+    # print(data)
+
+print(js)
+
+count = 0
+sum = 0
+
+js = json.loads(data)
+print('User count:', len(js["comments"]))
+for item in js["comments"]:
+    count = count + 1
+    sum = sum + int(item['count'])
+    print("name:", item['name'])
+    print("count:", item['count'])
+print("Count: ", count)
+print("sum: ", sum)
+
+### Graded Assignment 2: Finding Place id for Beloit College
+import urllib.error
+import json
+import ssl
+
+api_key = False
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else:
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
