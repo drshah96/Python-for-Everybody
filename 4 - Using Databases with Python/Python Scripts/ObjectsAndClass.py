6ce5bf7b00a41ba1f8eba6bf2ec2c52a4eef26ed
drshah96 <drshah96@gmail.com>
2020-06-12 15:16:57 -0400

    Using Databases with Python Week 1: Objects and Classes: Constructors and Destructors

--- 4 - Using Databases with Python/Python Scripts/ObjectsAndClass.py
+++ 4 - Using Databases with Python/Python Scripts/ObjectsAndClass.py
@@ -14,3 +14,25 @@ an.Party()
 
 print("Type", type(an))  # Type of attribute
 print("Dir:", dir(an))  # Functions Available
+
+
+###Constructor and Destructor
+class PartyAnimal:
+    x = 0
+
+    def __init__(self):
+        print("I am self Constructed")
+
+    def Party(self):
+        self.x = self.x + 1
+        print("So Far", self.x)
+
+    def __del__(self):
+        print("I am Destructed", self.x)
+
+
+an = PartyAnimal()
+an.Party()
+an.Party()
+an = 42
+print('an contains: ', an)
